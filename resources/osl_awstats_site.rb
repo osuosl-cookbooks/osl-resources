ff5b659d1546c35124c0c2d7b373c521c6a59edc
Violet Kurtz <vikurtz@osuosl.org>
2022-07-20 12:58:01 -0700

    awstats host fixes (partial)

--- resources/osl_awstats_site.rb
+++ resources/osl_awstats_site.rb
@@ -1,35 +1,35 @@
-resource_name :osl_awstats_host
-provides :osl_awstats_host
+resource_name :osl_awstats_site
+provides :osl_awstats_site
 unified_mode true
 
 default_action :create
 
-property :site_domain, String
+property :site_domain, String, name_property: true
 property :host_aliases, Array, default: []
 property :log_file, String, default: '*.log'
 property :only_files, Array, default: []
-property :using_ftp_dir, [true, false], default: false
+property :vsftp_logs, [true, false], default: false
 property :options, Hash, default: {}
 
 action :create do
   directory '/etc/awstats'
 
   template "/etc/awstats/awstats.#{new_resource.name}.conf" do
-    source 'awstats_host.conf.erb'
+    cookbook 'osl-resources'
+    source 'awstats_site.conf.erb'
     variables(
       site_domain: new_resource.site_domain,
       host_aliases: new_resource.host_aliases,
       log_file: new_resource.log_file,
       only_files: new_resource.only_files,
-      ftp: new_resource.using_ftp_dir,
+      vsftp: new_resource.vsftp_logs,
       options: new_resource.options
     )
-    action :create
   end
 end
 
 action :delete do
-  template "/etc/awstats/awstats.#{new_resource.name}.conf" do
+  file "/etc/awstats/awstats.#{new_resource.name}.conf" do
     action :delete
   end
 end
