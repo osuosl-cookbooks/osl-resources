6bef18a831ad6346520fdbff021f3b11646265ae
Violet Kurtz <vikurtz@osuosl.org>
2022-07-25 13:10:32 -0700

    Suffix now has its own property to enable/disable

--- resources/osl_awstats_site.rb
+++ resources/osl_awstats_site.rb
@@ -7,6 +7,7 @@ default_action :create
 property :site_domain, String, name_property: true
 property :host_aliases, Array, default: []
 property :log_file, String, default: '*.log'
+property :append_suffix, [true, false], default: false
 property :log_suffix, String, default: '.log'
 property :append_date, [true, false], default: false
 property :date_format, String, default: '-%YYYY-2%MM-2%DD-2'
@@ -23,7 +24,7 @@ action :create do
     source 'awstats_site.conf.erb'
     filename = new_resource.log_file
     filename.concat(new_resource.date_format) if new_resource.append_date
-    filename.concat(new_resource.log_suffix) if new_resource.append_date
+    filename.concat(new_resource.log_suffix) if new_resource.append_suffix
     variables(
       site_domain: new_resource.site_domain,
       host_aliases: new_resource.host_aliases,
