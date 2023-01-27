4c5eb4cfc4d04d07d3c375de9e2bfc93305ecd1f
oweng <oweng@scarlet.workstation.cass.oregonstate.edu>
2023-01-27 15:32:05 -0800

    Moved changes from main to this branch

--- resources/osl_mongodb.rb
+++ resources/osl_mongodb.rb
@@ -1,45 +1,26 @@
-resource_name :osl_virtualbox
-provides :osl_virtualbox
+resource_name :osl_mongodb
+provides :osl_mongodb
 unified_mode true
 
 default_action :install
 
-property :version, String, name_property: true
-
 action :install do
-  yum_repository 'virtualbox' do
-    baseurl 'http://download.virtualbox.org/virtualbox/rpm/el/$releasever/$basearch'
-    gpgcheck true
+
+  yum_repository 'mongodb-org-6.0' do
+    baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/6.0/x86_64/'
     repo_gpgcheck true
-    gpgkey 'https://www.virtualbox.org/download/oracle_vbox.asc'
-  end if platform_family?('rhel')
-
-  apt_repository 'virtualbox' do
-    components %w(contrib)
-    uri 'https://download.virtualbox.org/virtualbox/debian'
-    # Work around constant upstream issues pulling gpg keys
-    ignore_failure true
-    key %w(
-      https://www.virtualbox.org/download/oracle_vbox_2016.asc
-      https://www.virtualbox.org/download/oracle_vbox.asc
-    )
-  end if platform_family?('debian')
-
-  kernel_module 'kvm_amd' do
-    action [:unload, :blacklist]
+    gpgkey 'https://www.mongodb.org/static/pgp/server-6.0.asc'
   end
 
-  kernel_module 'kvm_intel' do
-    action [:unload, :blacklist]
+  package mongodb-org
+
+  template '/etc/mongod.conf' do
+    source 'mongod.erb'
   end
 
-  kernel_module 'kvm' do
-    action [:unload, :blacklist]
+
+  service 'mongod' do
+    action [ :enable, :start ]
   end
 
-  build_essential 'osl_virtualbox'
-
-  package virtualbox_packages
-
-  package "#{virtualbox_package_name}-#{new_resource.version}"
-end
+end if platform_family?('rhel')
