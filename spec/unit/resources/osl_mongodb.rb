bb5352b5997a4ed308f397f27715b3b24dfa4887
oweng <oweng@scarlet.workstation.cass.oregonstate.edu>
2023-01-27 15:43:34 -0800

    Wrote test for mongodb resource (WIP)

--- spec/unit/resources/osl_mongodb.rb
+++ spec/unit/resources/osl_mongodb.rb
@@ -1,60 +1,28 @@
 require 'spec_helper'
 
 describe 'osl_packagecloud_repo' do
-  recipe do
-    osl_virtualbox '6.1'
+  platform-family 'rhel'
+  cached(:subject) { chef_run }
+  step_into :osl_mongodb
+
+  it do
+    is_expected.to create_yum_repository('mongodb-org-6.0')
+      .with(
+        baseurl: 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/6.0/x86_64/',
+        repo_gpgcheck: true,
+        gpgcheck: true,
+        gpgkey: 'https://www.mongodb.org/static/pgp/server-6.0.asc'
+    )
+  end
+  it { is_expected.to install_package('mongodb-org') }
+
+  it do
+    is_expected.to create_template('/etc/mongod.conf')
+      .with(
+        source:'mongod.erb'
+    )
   end
 
-  context 'centos' do
-    platform 'centos'
-    cached(:subject) { chef_run }
-    step_into :osl_virtualbox
-
-    it do
-      is_expected.to create_yum_repository('virtualbox')
-        .with(
-          baseurl: 'http://download.virtualbox.org/virtualbox/rpm/el/$releasever/$basearch',
-          repo_gpgcheck: true,
-          gpgcheck: true,
-          gpgkey: 'https://www.virtualbox.org/download/oracle_vbox.asc'
-        )
-    end
-    it { is_expected.to unload_kernel_module('kvm_amd') }
-    it { is_expected.to unload_kernel_module('kvm_intel') }
-    it { is_expected.to unload_kernel_module('kvm') }
-    it { is_expected.to blacklist_kernel_module('kvm_amd') }
-    it { is_expected.to blacklist_kernel_module('kvm_intel') }
-    it { is_expected.to blacklist_kernel_module('kvm') }
-    it { is_expected.to install_build_essential('osl_virtualbox') }
-    it { is_expected.to install_package(%w(kernel-devel-4.18.0-193.6.3.el8_2.x86_64 elfutils-libelf-devel)) }
-    it { is_expected.to install_package('VirtualBox-6.1') }
-  end
-
-  context 'debian' do
-    platform 'debian'
-    cached(:subject) { chef_run }
-    step_into :osl_virtualbox
-
-    it do
-      is_expected.to add_apt_repository('virtualbox')
-        .with(
-          uri: 'https://download.virtualbox.org/virtualbox/debian',
-          ignore_failure: true,
-          key: %w(
-            https://www.virtualbox.org/download/oracle_vbox_2016.asc
-            https://www.virtualbox.org/download/oracle_vbox.asc
-          ),
-          components: %w(contrib)
-        )
-    end
-    it { is_expected.to unload_kernel_module('kvm_amd') }
-    it { is_expected.to unload_kernel_module('kvm_intel') }
-    it { is_expected.to unload_kernel_module('kvm') }
-    it { is_expected.to blacklist_kernel_module('kvm_amd') }
-    it { is_expected.to blacklist_kernel_module('kvm_intel') }
-    it { is_expected.to blacklist_kernel_module('kvm') }
-    it { is_expected.to install_build_essential('osl_virtualbox') }
-    it { is_expected.to install_package(%w(libelf-dev linux-headers-5.10.0-9-cloud-amd64)) }
-    it { is_expected.to install_package('virtualbox-6.1') }
-  end
+  it { is_expected.to enable_service('mongodb') }
+  it { is_expected.to start_service('mongodb') }
 end
