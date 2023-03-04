450350b1775b3717cd304c2f22033aaac05c5934
oweng <oweng@scarlet.workstation.cass.oregonstate.edu>
2023-03-03 16:11:56 -0800

    Fixed parameters test recipe

--- test/fixtures/cookbooks/osl-resources-test/recipes/osl_mongodb_parameters.rb
+++ test/fixtures/cookbooks/osl-resources-test/recipes/osl_mongodb_parameters.rb
@@ -1,9 +1,9 @@
-osl_mongodb '4.4' do
-  data_dir '/data/db/'
+osl_mongodb '6.0' do
+  data_dir '/var/lib/mongo2/'
   log_dest 'syslog'
   port 27019
   bind_ip '0.0.0.0'
   max_connections 5120
-  forking true
+  forking 'true'
   pid_file_path '/var/run/mongodb/mongod.pid'
 end
