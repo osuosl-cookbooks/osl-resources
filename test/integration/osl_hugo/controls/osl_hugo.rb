bce8c761c7ef339d39d08d8d3681d5774bdd8886
Paul Simko <paul@osuosl.org>
2024-01-19 13:57:04 -0800

    Replace version testing with hugo --version

--- test/integration/osl_hugo/controls/osl_hugo.rb
+++ test/integration/osl_hugo/controls/osl_hugo.rb
@@ -1,11 +1,4 @@
 control 'osl_hugo' do
-  describe directory('/opt/hugo-0.120.4') do
-    it { should exist }
-  end
-  describe directory('/opt/hugo') do
-    it { should exist }
-    its('link_path') { should cmp '/opt/hugo-0.120.4' }
-  end
   describe file('/opt/hugo/hugo') do
     it { should exist }
     its('size') { should be > 0 }
